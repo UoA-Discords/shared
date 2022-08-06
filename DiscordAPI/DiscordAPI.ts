da73d6ada5a91d8bdcaba8f6b10f4efb0bb3fdb3
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-08-06 20:05:12 +1200

    improve error objects

--- DiscordAPI/DiscordAPI.ts
+++ DiscordAPI/DiscordAPI.ts
@@ -1,69 +1,42 @@
 import axios from 'axios';
 import { APIInvite, APIPartialGuild, APIUser, RouteBases } from 'discord-api-types/v10';
 
-interface BaseHandleDiscordErrorResponse {
-    error: unknown | object;
-    isDiscordError: boolean;
-}
-
-interface HandleDiscordErrorResponseIs extends BaseHandleDiscordErrorResponse {
-    error: object;
-    isDiscordError: true;
-}
-
-interface HandleDiscordErrorResponseNot extends BaseHandleDiscordErrorResponse {
-    error: unknown;
-    isDiscordError: false;
-}
-
-export type HandleDiscordErrorResponse = HandleDiscordErrorResponseIs | HandleDiscordErrorResponseNot;
-
-/** Extracts the `{ message: "...", code: X }` portion of a Discord API 400 level response. */
-export function handleDiscordError(error: unknown): HandleDiscordErrorResponse {
-    if (!axios.isAxiosError(error)) return { error, isDiscordError: false };
-    try {
-        if (error.response?.data !== null && typeof error.response?.data === `object`) {
-            return { error: error.response.data, isDiscordError: true };
-        }
-    } catch (newError) {
-        /* don't care */
+export class DiscordAPIError {
+    public readonly data: object;
+    public constructor(data: object) {
+        this.data = data;
     }
-    return { error: new Error(error.message), isDiscordError: false };
+}
+
+/** Extracts the data of a Discord API 400 level response. */
+export function handleDiscordError(error: unknown): DiscordAPIError | unknown {
+    if (!axios.isAxiosError(error)) return error;
+    if (typeof error.response?.data !== `object`) return error;
+    if (error.response.data === null) return error;
+    return new DiscordAPIError(error.response.data);
 }
 
 export async function getInviteData(inviteCode: string): Promise<APIInvite> {
-    try {
-        const { data } = await axios.get<APIInvite>(`${RouteBases.api}/invites/${inviteCode}`, {
-            params: { with_expiration: true, with_counts: true },
-        });
-        return data;
-    } catch (error) {
-        throw handleDiscordError(error);
-    }
+    const { data } = await axios.get<APIInvite>(`${RouteBases.api}/invites/${inviteCode}`, {
+        params: { with_expiration: true, with_counts: true },
+    });
+    return data;
 }
 
 export async function getUserInfo(accessToken: string): Promise<APIUser> {
-    try {
-        const { data } = await axios.get<APIUser>(`${RouteBases.api}/users/@me`, {
-            headers: {
-                authorization: `Bearer ${accessToken}`,
-            },
-        });
-        return data;
-    } catch (error) {
-        throw handleDiscordError(error);
-    }
+    const { data } = await axios.get<APIUser>(`${RouteBases.api}/users/@me`, {
+        headers: {
+            authorization: `Bearer ${accessToken}`,
+        },
+    });
+    return data;
 }
 
 export async function getUserGuilds(accessToken: string): Promise<APIPartialGuild[]> {
-    try {
-        const { data } = await axios.get<APIPartialGuild[]>(`${RouteBases.api}/users/@me/guilds`, {
-            headers: {
-                authorization: `Bearer ${accessToken}`,
-            },
-        });
-        return data;
-    } catch (error) {
-        throw handleDiscordError(error);
-    }
+    const { data } = await axios.get<APIPartialGuild[]>(`${RouteBases.api}/users/@me/guilds`, {
+        headers: {
+            authorization: `Bearer ${accessToken}`,
+        },
+    });
+    return data;
 }
